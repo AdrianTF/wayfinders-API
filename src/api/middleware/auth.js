cc06970861225f4e8aa4c26e2c183ba258669c00
AdrianTF <adrian_tf96@hotmail.com>
2023-01-20 12:55:38 +0100

    Minor changes

--- src/api/middleware/auth.js
+++ src/api/middleware/auth.js
@@ -8,13 +8,9 @@ module.exports = (admin) => (req, res, next) => {
     if (!token) {
         return send.response404(res) //Should be res 403
     }
-
-    console.log("\nEl token: \n", token)
+    
     token = token.substring(7);
-    console.log("El token recortado:\n", token)
     let payload = service.verifyToken(token);
-    console.log("\nResultado de la validación: ", payload)
-
     if (payload && payload.admin)
         next();
     else if (payload && (admin === payload.admin))
